8bb45b0e47422de2d5a60253560290a36c5aaf0f
djolodjolo <tkd_djole@rocketmail.com>
2022-04-13 17:23:38 +0200

    Ready to deploy

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const userRoute = require('./routes/userRoute');
 const cors = require('cors');
+const path = require('path');
 
 //Connect DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -21,6 +22,16 @@ app.use(cors());
 app.use(express.json()) //body parser
 //Ovo je middleware
 app.use('/auth', userRoute)
+
+//Serve build
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, ()=> console.log("Server is running!"))
